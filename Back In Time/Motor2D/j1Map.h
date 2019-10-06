f0c783dc4c4989ce1704cf50b12dad4db0c1cdff
pablogalve <eduinvestor100@gmail.com>
2019-10-06 18:32:22 +0200

    basic project created with all modules

---
+++ Back In Time/Motor2D/j1Map.h
@@ -0,0 +1,99 @@
+#ifndef __j1MAP_H__
+#define __j1MAP_H__
+
+#include "PugiXml/src/pugixml.hpp"
+#include "p2List.h"
+#include "p2Point.h"
+#include "j1Module.h"
+
+// TODO 1: Create a struct for the map layer
+// ----------------------------------------------------
+
+	// TODO 6: Short function to get the value of x,y
+
+
+
+// ----------------------------------------------------
+struct TileSet
+{
+	// TODO 7: Create a method that receives a tile id and returns it's Rect
+
+	p2SString			name;
+	int					firstgid;
+	int					margin;
+	int					spacing;
+	int					tile_width;
+	int					tile_height;
+	SDL_Texture*		texture;
+	int					tex_width;
+	int					tex_height;
+	int					num_tiles_width;
+	int					num_tiles_height;
+	int					offset_x;
+	int					offset_y;
+};
+
+enum MapTypes
+{
+	MAPTYPE_UNKNOWN = 0,
+	MAPTYPE_ORTHOGONAL,
+	MAPTYPE_ISOMETRIC,
+	MAPTYPE_STAGGERED
+};
+// ----------------------------------------------------
+struct MapData
+{
+	int					width;
+	int					height;
+	int					tile_width;
+	int					tile_height;
+	SDL_Color			background_color;
+	MapTypes			type;
+	p2List<TileSet*>	tilesets;
+	// TODO 2: Add a list/array of layers to the map!
+};
+
+// ----------------------------------------------------
+class j1Map : public j1Module
+{
+public:
+
+	j1Map();
+
+	// Destructor
+	virtual ~j1Map();
+
+	// Called before render is available
+	bool Awake(pugi::xml_node& conf);
+
+	// Called each loop iteration
+	void Draw();
+
+	// Called before quitting
+	bool CleanUp();
+
+	// Load new map
+	bool Load(const char* path);
+
+	// TODO 8: Create a method that translates x,y coordinates from map positions to world positions
+
+private:
+
+	bool LoadMap();
+	bool LoadTilesetDetails(pugi::xml_node& tileset_node, TileSet* set);
+	bool LoadTilesetImage(pugi::xml_node& tileset_node, TileSet* set);
+	// TODO 3: Create a method that loads a single laye
+	// bool LoadLayer(pugi::xml_node& node, MapLayer* layer);
+
+public:
+
+	MapData data;
+
+private:
+
+	pugi::xml_document	map_file;
+	p2SString			folder;
+	bool				map_loaded;
+};
+
+#endif // __j1MAP_H__
---
+++ Back In Time/Motor2D/j1Scene.cpp
@@ -0,0 +1,93 @@
+#include "p2Defs.h"
+#include "p2Log.h"
+#include "j1App.h"
+#include "j1Input.h"
+#include "j1Textures.h"
+#include "j1Audio.h"
+#include "j1Render.h"
+#include "j1Window.h"
+#include "j1Map.h"
+#include "j1Scene.h"
+
+j1Scene::j1Scene() : j1Module()
+{
+	name.create("scene");
+}
+
+// Destructor
+j1Scene::~j1Scene()
+{}
+
+// Called before render is available
+bool j1Scene::Awake()
+{
+	LOG("Loading Scene");
+	bool ret = true;
+
+	return ret;
+}
+
+// Called before the first frame
+bool j1Scene::Start()
+{
+	App->map->Load("hello2.tmx");
+	return true;
+}
+
+// Called each loop iteration
+bool j1Scene::PreUpdate()
+{
+	return true;
+}
+
+// Called each loop iteration
+bool j1Scene::Update(float dt)
+{
+	if(App->input->GetKey(SDL_SCANCODE_L) == KEY_DOWN)
+		App->LoadGame();
+
+	if(App->input->GetKey(SDL_SCANCODE_S) == KEY_DOWN)
+		App->SaveGame();
+
+	if(App->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
+		App->render->camera.y -= 1;
+
+	if(App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
+		App->render->camera.y += 1;
+
+	if(App->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
+		App->render->camera.x -= 1;
+
+	if(App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
+		App->render->camera.x += 1;
+
+	//App->render->Blit(img, 0, 0);
+	App->map->Draw();
+
+	p2SString title("Map:%dx%d Tiles:%dx%d Tilesets:%d",
+					App->map->data.width, App->map->data.height,
+					App->map->data.tile_width, App->map->data.tile_height,
+					App->map->data.tilesets.count());
+
+	App->win->SetTitle(title.GetString());
+	return true;
+}
+
+// Called each loop iteration
+bool j1Scene::PostUpdate()
+{
+	bool ret = true;
+
+	if(App->input->GetKey(SDL_SCANCODE_ESCAPE) == KEY_DOWN)
+		ret = false;
+
+	return ret;
+}
+
+// Called before quitting
+bool j1Scene::CleanUp()
+{
+	LOG("Freeing scene");
+
+	return true;
+}
