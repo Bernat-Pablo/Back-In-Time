962c5cbb37809fbfedeba8b1d6b0e7d37a0e1118
Berniix <djberniix@gmail.com>
2019-10-08 23:39:00 +0200

    Folders ReOrganitzation & Map Update

--- Back In Time/Game/Assets/tiles/BasicTiles.tsx
+++ Back In Time/Game/Assets/tiles/BasicTiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.4" name="Basic Tiles Only" tilewidth="16" tileheight="16" tilecount="4816" columns="86">
- <image source="../Documents/GitHub/Back-In-Time/Assets/16x16 Fantasy Platformer Pack/Tilesets/Basic Tiles Only.png" width="1376" height="896"/>
+ <image source="Basic Tiles Only.png" width="1376" height="896"/>
 </tileset>
