33eb4a61d7f0a9ebb853c71b82996de712ac13f9
Berniix <djberniix@gmail.com>
2019-10-11 13:31:57 +0200

    animation pushback idle done

--- BackInTime/Motor2D/j1Character.h
+++ BackInTime/Motor2D/j1Character.h
@@ -3,6 +3,7 @@
 
 #include "j1Character.h"
 #include "j1App.h"
+#include "j1Animation.h"
 #include "j1Textures.h"
 #include "j1Audio.h"
 #include "j1Input.h"
@@ -31,6 +32,8 @@ public:
 	Animation walk_atack;
 	Animation atack1;
 	Animation atack2;
+
+	SDL_Texture* texture;
 
 
 };
---
+++ BackInTime/Motor2D/j1Player.cpp
@@ -0,0 +1,19 @@
+#include "j1App.h"
+#include "j1Textures.h"
+#include "j1Input.h"
+#include "j1Render.h"
+#include "j1Player.h"
+#include "j1Audio.h"
+#include "j1Character.h"
+#include "j1Wot.h"
+#include "j1Collision.h"
+
+
+
+bool j1Player::Update() {
+
+	if (App->input->GetKey(SDL_SCANCODE_D)==KEY_REPEAT) {
+
+	}
+
+}
