fb498b77420cae0df527ae0fd8000f61cbb30a4b
Pablo <eduinvestor100@gmail.com>
2019-11-26 01:17:45 +0100

    entity types

--- BackInTime/Motor2D/EntityManager.h
+++ BackInTime/Motor2D/EntityManager.h
@@ -2,20 +2,15 @@
 #define __j1ENTITYMANAGER_H__
 
 #include "j1Module.h"
-#include "p2List.h"
 #include "p2Point.h"
-#include "j1Player.h"
+#include "j1Animation.h"
 
 class Entity;
 
-enum timeState
-{
-	NO_STATE,
-	NORMAL,
-	SLOWING,
-	STOPPED,
-	FASTENING,
-	RECENTLY_STOPPED
+enum Entity_Types {
+	PLAYER,
+	WALKING_ENEMY,
+	FLYING_ENEMY,
 };
 
 class EntityManager : public j1Module
@@ -24,11 +19,15 @@ public:
 	EntityManager();
 	~EntityManager();
 
-	bool Awake(pugi::xml_node& config);
-	bool Start();
-	bool Update(float dt);
-	bool PostUpdate();
-	bool CleanUp();
+	virtual bool Awake(pugi::xml_node& config);
+	virtual bool Start();
+	virtual bool Update(float dt);
+	virtual bool PostUpdate();
+	virtual bool CleanUp();
+
+	//TODO 
+	//Function CreateEntity
+	//Function DestroyEntity
 };
 
 #endif // !__j1ENTITYMANAGER_H__
