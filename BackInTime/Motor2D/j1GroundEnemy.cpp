6ba51cee6de8824ac47b37be7c251ea63db68132
Bernat Casanas <bernatcasanas@gmail.com>
2019-11-30 00:35:43 +0100

    state machine rino DONE

--- BackInTime/Motor2D/j1GroundEnemy.cpp
+++ BackInTime/Motor2D/j1GroundEnemy.cpp
@@ -48,30 +48,83 @@ j1GroundEnemy::j1GroundEnemy() : j1Entity(entityTypes::GROUND_ENEMY)
 
 }
 
-bool j1GroundEnemy::Awake(pugi::xml_node&)
+bool j1GroundEnemy::Awake(pugi::xml_node& config)
 {
+	config = App->GetConfig();
+	config = config.child("entityManager").child("groundEnemy");
 
+	//variables definition
+	
 	return true;
 }
 
 bool j1GroundEnemy::Start()
 {
 
+	spritesheet_entity = App->tex->Load("character/enemies_spritesheet.png");
+	debug_tex = App->tex->Load("maps/pathRect.png");
+
+	//state = entityStates::FLY;
+	//current_animation = &fly;
+
+	collider_entity = App->collision->AddCollider(current_animation->GetCurrentFrame(), COLLIDER_GROUND_ENEMY, "rino", (j1Module*)App->groundEnemy); //a collider to start
+
 	return true;
 }
 
 bool j1GroundEnemy::PreUpdate()
 {
+	switch (state)
+	{
+	case entityStates::IDLE:
+
+		break;
+	case entityStates::RUN_FORWARD:
+
+		break;
+	case entityStates::RUN_BACKWARD:
+
+		break;
+	case entityStates::STUNNED:
+
+		break;
+	case entityStates::HIT:
+
+		break;
+	}
+
 	return true;
 }
 
 bool j1GroundEnemy::Update(float dt)
 {
+	switch (state)
+	{
+	case entityStates::IDLE:
+
+		break;
+	case entityStates::RUN_FORWARD:
+
+		break;
+	case entityStates::RUN_BACKWARD:
+
+		break;
+	case entityStates::STUNNED:
+
+		break;
+	case entityStates::HIT:
+
+		break;
+	}
+
 	return true;
 }
 
 bool j1GroundEnemy::CleanUp()
 {
+	App->tex->UnLoad(spritesheet_entity);
+	App->tex->UnLoad(debug_tex);
+
 	return true;
 }
 
@@ -82,12 +135,27 @@ void j1GroundEnemy::OnCollision(Collider* c1, Collider* c2)
 
 void j1GroundEnemy::calculate_path()
 {
+	iPoint origin = App->map->WorldToMap(App->player->position.x, App->player->position.y);
 
+	iPoint p = App->render->ScreenToWorld(position.x, position.y);
+	p = App->map->WorldToMap(position.x, position.y);
+	if (App->player->position.x - position.x <= 160 && position.x - App->player->position.x >= -160) {
+		App->pathfinding->CreatePath(origin, p);
+		if (set_path == true) {
+			check_path_toMove();
+		}
+	}
 }
 
 void j1GroundEnemy::blit_path()
 {
+	const p2DynArray<iPoint>* path = App->pathfinding->GetLastPath();
 
+	for (uint i = 0; i < path->Count(); ++i)
+	{
+		iPoint pos = App->map->MapToWorld(path->At(i)->x, path->At(i)->y);
+		App->render->Blit(debug_tex, pos.x, pos.y);
+	}
 }
 
 void j1GroundEnemy::check_path_toMove()
