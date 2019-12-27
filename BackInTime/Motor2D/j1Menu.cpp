3807a2b485628a96b73e9b37474426a0d1c65622
Berniix <djberniix@gmail.com>
2019-12-27 15:43:33 +0100

    Update j1Menu.cpp

---
+++ BackInTime/Motor2D/j1Menu.cpp
@@ -0,0 +1,25 @@
+#include "j1Menu.h"
+
+j1Menu::j1Menu()
+{
+}
+
+bool j1Menu::Awake(pugi::xml_node& conf)
+{
+	return false;
+}
+
+bool j1Menu::Start()
+{
+	return false;
+}
+
+bool j1Menu::Update(float dt)
+{
+	return false;
+}
+
+bool j1Menu::CleanUp()
+{
+	return false;
+}
