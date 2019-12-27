cecaed9e064ae87ecdb0b1ff1bafff8facd672d7
eduinvestor <eduinvestor100@gmail.com>
2019-12-28 00:37:00 +0100

    [fix] img Update working in menu

--- BackInTime/Motor2D/j1App.cpp
+++ BackInTime/Motor2D/j1App.cpp
@@ -274,9 +274,10 @@ bool j1App::DoUpdate()
 	for(item = modules.start; item != NULL && ret == true; item = item->next)
 	{
 		pModule = item->data;
-		if (pModule->name != "menu" && App->menu->menuAble) {
-			continue;
-		}
+		if (pModule->name != "menu" && App->menu->menuAble)
+			if (pModule->name == "menu" || pModule->name == "gui")
+				LOG("");
+			else continue;
 		if(pModule->active == false) {
 			continue;
 		}
