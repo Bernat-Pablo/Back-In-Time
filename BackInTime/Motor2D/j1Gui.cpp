376212af484448c1d80aed5fb692af889fd39c16
Berniix <djberniix@gmail.com>
2019-12-28 16:01:11 +0100

    ui element font WOKING :astonished:

--- BackInTime/Motor2D/j1Gui.cpp
+++ BackInTime/Motor2D/j1Gui.cpp
@@ -27,7 +27,7 @@ bool j1Gui::Start()
 
 	ret = true;
 	ui_spritesheet = App->tex->Load(ui_spritesheet_path.GetString());
-	App->fonts->Load("fonts/small_white_font.png", "abcdefghiklmnoprstuwy'.0123456789", 1, 8,8,33);
+	App->fonts->Load("fonts/small_white_font.png", " abcdefghiklmnoprstuwy'.0123456789", 1, 8,8,34);
 	//ui_spritesheet = App->tex->Load("character/coin_spritesheet.png");
 
 	return ret;
