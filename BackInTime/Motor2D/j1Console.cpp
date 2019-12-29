7d62cf77e24eaa07bfc47275e61b94b896c8fd84
Berniix <djberniix@gmail.com>
2019-12-29 22:04:19 +0100

    console creation

--- BackInTime/Motor2D/j1Console.cpp
+++ BackInTime/Motor2D/j1Console.cpp
@@ -2,10 +2,40 @@
 
 j1Console::j1Console()
 {
-
+	god = "god_mode";
+	quit = "quit";
+	fps = "fps";
+	map = "map";
+	list = "god_mode,quit,fps<>,map<>";
 }
 
-j1Console::~j1Console()
+bool j1Console::Awake()
 {
-
+	return true;
 }
+
+bool j1Console::Start()
+{
+	return true;
+}
+
+bool j1Console::PreUpdate(float dt)
+{
+	return true;
+}
+
+bool j1Console::Update(float dt)
+{
+	return true;
+}
+
+bool j1Console::PostUpdate(float dt)
+{
+	return true;
+}
+
+bool j1Console::CleanUp()
+{
+	return true;
+}
+
--- BackInTime/Motor2D/j1Console.h
+++ BackInTime/Motor2D/j1Console.h
@@ -1,10 +1,36 @@
 #ifndef J1_CONSOLE_H
 #define J1_CONSOLE_H
 
-class j1Console
-{
-	j1Console();
-	~j1Console();
+#include "j1App.h"
+#include "j1Module.h"
+#include "p2SString.h"
 
+class j1Console : public j1Module
+{
+public:
+	j1Console();
+	~j1Console() {};
+
+	// Called before render is available
+	bool Awake();
+
+	// Called before the first frame
+	bool Start();
+
+	// Called before all Updates
+	bool PreUpdate(float dt);
+
+	// Called each loop iteration
+	bool Update(float dt);
+
+	// Called before all Updates
+	bool PostUpdate(float dt);
+
+	// Called before quitting
+	bool CleanUp();
+
+private:
+	bool open = false;
+	p2SString god, quit, fps, map, list;
 };
 #endif // !J1_CONSOLE_H
