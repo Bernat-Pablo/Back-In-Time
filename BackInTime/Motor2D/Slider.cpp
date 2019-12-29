214e4f54dbf9d28a94e4396a2edf6c8e051cdea1
Berniix <djberniix@gmail.com>
2019-12-29 11:46:16 +0100

    all functions slider

---
+++ BackInTime/Motor2D/Slider.cpp
@@ -0,0 +1,22 @@
+#include "Slider.h"
+
+bool Slider::Start()
+{
+	return true;
+}
+
+bool Slider::Update(float dt)
+{
+	return true;
+}
+
+bool Slider::PostUpdate()
+{
+	return true;
+}
+
+bool Slider::CleanUp()
+{
+	
+	return true;
+}
